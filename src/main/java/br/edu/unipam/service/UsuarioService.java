b832a8fbf46ebec0215e5154d53b1bb9b4c3be3b
JrAdilson <adilsonagj@unipam.edu.br>
2021-03-18 21:03:38 -0300

    Service Tier (EntityManager)

--- src/main/java/br/edu/unipam/service/UsuarioService.java
+++ src/main/java/br/edu/unipam/service/UsuarioService.java
@@ -1,38 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.edu.unipam.service;
-
 import br.edu.unipam.entity.Usuario;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-
 /**
  *
- * @author JrAdilson
+ * @author felipelsouza
  */
-
 public class UsuarioService {
   @PersistenceContext(name = "pu_todo")
   private EntityManager entityManager;
-
   //inserir
   public Usuario salvarUsuario(Usuario usuario) {
     entityManager.persist(usuario);
     return usuario;
   }
-
   //Encontrar usuário por ID
   public Usuario localizarPorId(Long id) {
     Usuario userBd = entityManager.find(Usuario.class, id);
     return userBd;
   }
-
   //Remover
   public void remover(Long id) {
     Usuario user = localizarPorId(id);
@@ -40,18 +30,15 @@ public class UsuarioService {
       entityManager.remove(user);
     }
   }
-
   //Editar
   public Usuario editar(Usuario usuario) {
     Usuario userBd = localizarPorId(usuario.getId());
-
     if (userBd != null) {
       entityManager.merge(usuario);
       return usuario;
     }
     return null;
   }
-
   //Listar Todos os usuários
   public List<Usuario> listarTodos ()
   {
